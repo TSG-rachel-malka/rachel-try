df683c24a6bc2fe7e1b391e267b2be75aa9de5f0
Malka Weingarten <P0028371@ness-tech.co.il>
2020-04-06 12:34:41 +0300

    fix bug in angular

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,34 @@ const incidentRoute = require('./routes/incident');
 
 const app = express();
 
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+     host: 'mydb.com', 
+     user:'myUser', 
+     password: 'myPassword',
+     connectionLimit: 5
+});
+console.log()
+async function asyncFunction() {
+  let conn;
+  try {
+	conn = await pool.getConnection();
+	const rows = await conn.query("SELECT 1 as val");
+	console.log(rows); //[ {val: 1}, meta: ... ]
+	const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+	console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+
+  } catch (err) {
+    console.log('err');
+    console.log(err);
+	throw err;
+  } finally {
+	if (conn){ 
+    console.log('cnn');
+    console.log(cnn);
+    return conn.end();}
+  }
+}
 /*mongoose.connect("mongodb+srv://malka:"+
                   process.env.MONGO_ATLAS_PW +
                   "@cluster0-bik5o.mongodb.net/test"
